687d25f0538d7eb695cc86661f62064dfdd92f82
Aadidev <aadidev1006@gmail.com>
2025-03-03 21:47:42 +0530

    feat : added graphql playground for api and added JWT Extension for chowkidar

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 URL configuration for api project.
 
@@ -18,17 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-=======
 from django.contrib import admin
 from django.urls import path
 from strawberry.django.views import GraphQLView
 from .schema import schema
 from chowkidar.view import auth_enabled_view
+from strawberry.django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("auth/", auth_enabled_view(GraphQLView.as_view(schema=schema))),
->>>>>>> backend-freshstart
+    path("graphql/", GraphQLView.as_view(schema=schema)),
 ]
