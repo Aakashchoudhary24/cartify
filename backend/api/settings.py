cf6f834d2c4d8481d012ff18d5483f9564687848
Aakash <aakashchoudhary724@gmail.com>
2025-03-11 18:41:51 +0530

    fix : remove unwanted file .DS_store

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -102,7 +102,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER':  os.getenv('DB_USER'),
-       'PASSWORD':  os.getenv('DB_PASSWORD'),
+        'PASSWORD':  os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'), 
         'PORT': os.getenv('DB_PORT'),
     }
