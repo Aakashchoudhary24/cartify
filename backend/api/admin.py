653adb80290e46eca9cd518a00010d8c9394c0d7
dilip27m <mdilip2909@gmail.com>
2025-03-12 22:04:15 +0530

    Update:fixed the bugs the changes were not visible in admin page

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Category, Product, Profile, Cart, CartItem, Order, OrderItem
 
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
--- backend/api/settings.py
+++ backend/api/settings.py
@@ -19,6 +19,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -68,7 +69,7 @@ ROOT_URLCONF = 'api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -142,7 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
