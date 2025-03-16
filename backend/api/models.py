cb02f13615348015f89bd1e19cb91f3fb929efd9
Aadidev <aadidev1006@gmail.com>
2025-03-16 16:08:22 +0530

    feat : updated models.py to fetch data from authentication_customuser

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-# from settings import AUTH_USER_MODEL
+from settings import AUTH_USER_MODEL
 
 # Category Model
 class Category(models.Model):
@@ -42,7 +42,7 @@ class Profile(models.Model):
 
 # Cart Model
 class Cart(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="carts")
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="carts")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
